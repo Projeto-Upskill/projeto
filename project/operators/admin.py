6898648d1e3b8421cc9334fa245751c6e848bc2e
inessdecastro <inessdecastro@gmail.com>
2024-04-02 16:14:57 +0100

    operators update 2.0

--- project/operators/admin.py
+++ project/operators/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Operators
 
-# Register your models here.
+
+@admin.register(Operators)
+class OperatorsAdmin(admin.ModelAdmin):
+    list_display = ['id_operator', 'name', 'email', 'birth_date', 'active']
