5f46a3ab125d141ce3c52628baad8ef48bc2cb2e
inessdecastro <inessdecastro@gmail.com>
2024-04-02 15:53:02 +0100

    Operators update

---
+++ project/operators/urls.py
@@ -0,0 +1,23 @@
+from django.contrib import admin
+from django.urls import path
+from .views import (OperatorsCreateView,
+                    OperatorsListView,
+                    OperatorsUpdateView,
+                    OperatorsDeleteView,
+                    OperatorsIndex,
+                    MenuCustomers,
+                    MenuPackages,
+                    MenuDiscounts)
+
+app_name = 'Operators'
+
+urlpatterns = [
+    path("", OperatorsIndex.as_view(), name="operator_index"),
+    path("create", OperatorsCreateView.as_view(), name="operator_create"),
+    path("list", OperatorsListView.as_view(), name='operator_list'),
+    path("<int:id_operator>/update", OperatorsUpdateView.as_view(), name='operator_update'),
+    path("<int:id_Operator>/delete", OperatorsDeleteView.as_view(), name='operator_delete'),
+    path("menu_customers", MenuCustomers.as_view(), name='menu_customers'),
+    path("menu_packages", MenuPackages.as_view(), name='menu_packages'),
+    path("menu_discounts", MenuDiscounts.as_view(), name='menu_discounts')
+]
--- project/operators/views.py
+++ project/operators/views.py
@@ -1,3 +1,72 @@
 from django.shortcuts import render
+from django.views.generic import CreateView, ListView, UpdateView, DeleteView, TemplateView
+from .forms import OperatorsForm
+from .models import Operators
+from django.shortcuts import get_object_or_404
 
-# Create your views here.
+class OperatorsCreateView(CreateView):
+    template_name = 'operators_create.html'
+    form_class = OperatorsForm
+    success_url = reverse_lazy('menu_operators')
+
+    def form_valid(self, form):
+        form.instance.name = form.cleaned_data["name"]
+        form.instance.email = form.cleaned_data["email"]
+        form.instance.birth_date = form.cleaned_data["birth_date"]
+        form.instance.admission_date = form.cleaned_data["admission_date"]
+        form.instance.active = form.cleaned_data["active"]
+
+        return super().form_valid(form)
+    
+class OperatorsListView(ListView):
+    model = Operators
+    template_name = 'operators_list.html'
+    context_object_name = 'operators_list'
+
+
+class OperatorsUpdateView(UpdateView):
+    template_name = 'operators_create.html'
+    form_class = OperatorsForm
+    success_url = reverse_lazy("operators_list")
+
+    def get_object(self):
+        id_operator = self.kwargs.get('id_operator')
+        return get_object_or_404(Operators, id_operator=id_operator)
+
+    def form_valid(self, form):
+        form.instance.name = form.cleaned_data["name"]
+        form.instance.email = form.cleaned_data["email"]
+        form.instance.birth_date = form.cleaned_data["birth_date"]
+        form.instance.active = form.cleaned_data["active"]
+
+        return super().form_valid(form)
+
+
+class OperatorsDeleteView(DeleteView):
+    model = Operators
+    template_name = 'operators_confirm_delete.html'
+
+    def get_object(self, queryset=None):
+        id_operator = self.kwargs.get('id_operator')
+        return get_object_or_404(Operators, id_operator=id_operator)
+
+    def get_success_url(self):
+        return reverse_lazy('operators_list')
+
+
+class OperatorsIndex(TemplateView):
+    template_name = 'index_operators.html'
+
+
+class MenuCustomers(TemplateView):
+    template_name = 'menu_customers.html'
+
+
+class MenuPackages(TemplateView):
+    template_name = 'menu_packages.html'
+
+
+class MenuDiscounts(TemplateView):
+    template_name = 'menu_discounts.html'
+    
+    
