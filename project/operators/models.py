ceb30ee5ff653d7f2d5ecb0bf764c277efa37bb1
inessdecastro <inessdecastro@gmail.com>
2024-04-02 14:50:54 +0100

    operators models update

--- project/operators/models.py
+++ project/operators/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class operators(models.Model):
+	id_operator = models.AutoField(primary_key=True, verbose_name='id_operator')
+	name = models.CharField(max_length=500)
+	email = models.EmailField(max_length=300)
+	birth_date = models.DateField()
+	active = models.BooleanField()
+
+	class Meta:
+		db_table = 'operators'
+
+	
