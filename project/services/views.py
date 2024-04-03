be60852dedff35edd2611107651b1145fc184be2
Guilherme <guilhermejft@gmail.com>
2024-04-03 16:22:10 +0100

    upload views.py

--- project/services/views.py
+++ project/services/views.py
@@ -3,26 +3,26 @@ from django.shortcuts import render
 # Create your views here.
 from django.views.generic import CreateView, ListView, UpdateView, DeleteView
 from django.urls import reverse_lazy
-from .models import ServiceType, Service
-from .forms import ServiceTypeForm, ServiceForm
+from .models import ServiceType, Service, ServiceDiscount
+from .forms import ServiceTypeForm, ServiceForm, ServiceDiscountForm
 from django.shortcuts import get_object_or_404
 
 
 class ServiceTypeListView(ListView):
     model = ServiceType
-    template_name = 'services/templates/service_type_list.html'
+    template_name = 'service_type_list.html'
     context_object_name = 'service_types'
 
 class ServiceTypeCreateView(CreateView):
     model = ServiceType
     form_class = ServiceTypeForm
-    template_name = 'services/templates/service_type_form.html'
+    template_name = 'service_type_form.html'
     success_url = reverse_lazy('services:service_type_list')
 
 class ServiceTypeUpdateView(UpdateView):
     model = ServiceType
     form_class = ServiceTypeForm
-    template_name = 'services/templates/service_type_form.html'
+    template_name = 'service_type_form.html'
     success_url = reverse_lazy('services:service_type_list')
 
     def get_object(self):
@@ -31,7 +31,7 @@ class ServiceTypeUpdateView(UpdateView):
 
 class ServiceTypeDeleteView(DeleteView):
     model = ServiceType
-    template_name = 'services/templates/service_type_confirm_delete.html'
+    template_name = 'service_type_confirm_delete.html'
     success_url = reverse_lazy('services:service_type_list')
 
     def get_object(self):
@@ -42,19 +42,19 @@ class ServiceTypeDeleteView(DeleteView):
 
 class ServiceListView(ListView):
     model = Service
-    template_name = 'services/service_list.html'
+    template_name = 'service_list.html'
     context_object_name = 'service'
 
 class ServiceCreateView(CreateView):
     model = Service
     form_class = ServiceForm
-    template_name = 'services/service_form.html'
+    template_name = 'service_form.html'
     success_url = reverse_lazy('services:service_list')
 
 class ServiceUpdateView(UpdateView):
     model = Service
     form_class = ServiceForm
-    template_name = 'services/service_form.html'
+    template_name = 'service_form.html'
     success_url = reverse_lazy('services:service_list')
 
     def get_object(self):
@@ -63,11 +63,42 @@ class ServiceUpdateView(UpdateView):
 
 class ServiceDeleteView(DeleteView):
     model = Service
-    template_name = 'services/service_confirm_delete.html'
+    template_name = 'service_confirm_delete.html'
     success_url = reverse_lazy('services:service_list')
 
     def get_object(self):
         id = self.kwargs.get('id_service')
         return get_object_or_404(ServiceType, id_service=id)
 
+#Let's create views for ServiceDiscount
+
+class ServiceDiscountListView(ListView):
+    model = ServiceDiscount
+    template_name = 'service_discount_list.html'
+    context_object_name = 'service_discount'
+
+class ServiceDiscountCreateView(CreateView):
+    model = ServiceDiscount
+    form_class = ServiceDiscountForm
+    template_name = 'service_discount_form.html'
+    success_url = reverse_lazy('services:service_discount_list')
+
+class ServiceDiscountUpdateView(UpdateView):
+    model = ServiceDiscount
+    form_class = ServiceDiscountForm
+    template_name = 'service_discount_form.html'
+    success_url = reverse_lazy('services:service_discount_list')
+
+    def get_object(self):
+        id = self.kwargs.get('id_service_discount')
+        return get_object_or_404(ServiceType, id_service_discount=id)
+
+class ServiceDiscountDeleteView(DeleteView):
+    model = ServiceDiscount
+    template_name = 'service_discount_confirm_delete.html'
+    success_url = reverse_lazy('services:service_discount_list')
+
+    def get_object(self):
+        id = self.kwargs.get('id_service_discount')
+        return get_object_or_404(ServiceType, id_service_discount=id)
 
