d6c94017575ea9eafef22796d38fe04af53290df
Guilherme <guilhermejft@gmail.com>
2024-04-03 12:37:04 +0100

    Added service functionalities, still need to perfect the presentation in admin

--- project/services/forms.py
+++ project/services/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from .models import ServiceType
+from .models import Service
 
 class ServiceTypeForm(forms.ModelForm):
     class Meta:
@@ -10,4 +11,17 @@ class ServiceTypeForm(forms.ModelForm):
         }
         widgets = {
             'service_name': forms.TextInput(attrs={'class': 'form-control'}),
+        }
+
+class ServiceForm(forms.ModelForm):
+    class Meta:
+        model = Service
+        fields = ['id_service_type', 'active']
+        labels = {
+            'id_service_type': 'Service Type',
+            'active': 'Active',
+        }
+        widgets = {
+            'id_service_type': forms.Select(attrs={'class': 'form-control'}),
+            'active': forms.CheckboxInput(attrs={'class': 'form-check-input'}),
         }
