6b17008298d0c73c7672172c498f3b7f70735f00
henriquealvesgonzaga87 <henriquealves87@hotmail.com>
2024-04-04 17:23:41 +0100

    creates the customer create and list view, temp, form and urls

--- project/customers/urls.py
+++ project/customers/urls.py
@@ -1,13 +1,19 @@
 from django.urls import path
-from . import views
+from .views import (CustomerCreateView,
+                    CustomerListView,
+                    view_customer_package,
+                    view_customer_unique_service,
+                    update_customer_data)
 
 app_name = 'customers'
 
 urlpatterns = [
-    path('package/', views.view_customer_package, name='view_customer_package'),
-    path('unique_service/', views.view_customer_unique_service, name='view_customer_unique_service'),
+    path("create", CustomerCreateView.as_view(), name='create_customer'),
+    path("list", CustomerListView.as_view(), name='customer_list'),
+    path('package/', view_customer_package, name='view_customer_package'),
+    path('unique_service/', view_customer_unique_service, name='view_customer_unique_service'),
     #path('customer/promotions/', views.view_customer_promotions, name='view_customer_promotions'),
-    path('update/', views.update_customer_data, name='update_customer_data'),
+    path('update/', update_customer_data, name='update_customer_data'),
     #path('available/packages/', views.view_available_packages, name='view_available_packages'),
     # path('available/unique_services/', views.view_available_unique_services, name='view_available_unique_services'),
     # path('available/promotions/', views.view_available_promotions, name='view_available_promotions'),
--- project/customers/views.py
+++ project/customers/views.py
@@ -2,17 +2,44 @@ from django.contrib import messages
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Customer, Address, PostalCode, City
 from .forms import CustomerForm
+from django.views.generic import CreateView, ListView
+from django.urls import reverse_lazy
+
+
+class CustomerCreateView(CreateView):
+    template_name = 'customer_create.html'
+    form_class = CustomerForm
+    success_url = reverse_lazy('administrator:administrator_index')
+
+    def form_valid(self, form):
+        form.name = form.cleaned_data["name"]
+        form.tax_number = form.cleaned_data["tax_number"]
+        form.email = form.cleaned_data["email"]
+        form.birth_date = form.cleaned_data["birth_date"]
+        form.active = form.cleaned_data["active"]
+
+        return super().form_valid(form)
+
+
+class CustomerListView(ListView):
+    template_name = 'customer_list.html'
+    model = Customer
+    context_object_name = 'customer_list'
+
 
 def get_customer(request):
     return get_object_or_404(Customer, user=request.user)
+
 
 def view_customer_package(request):
     customer = get_customer(request)
     return render(request, 'clientes/customer_package.html', {'package': customer.package})
 
+
 def view_customer_unique_service(request):
     customer = get_customer(request)
     return render(request, 'clientes/customer_unique_service.html', {'unique_service': customer.unique_service})
+
 
 # def view_customer_promotions(request):
 #     customer = get_customer(request)
