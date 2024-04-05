54cd0016e0879609e42e203919b708759461743e
inessdecastro <inessdecastro@gmail.com>
2024-04-05 11:46:57 +0100

    Packages Admin

--- project/packages/admin.py
+++ project/packages/admin.py
@@ -1,12 +1,20 @@
-#from django.contrib import admin
-#from .models import Package, PackageDiscount
+from django.contrib import admin
+from .models import Package, PackageDiscount, PackageDiscountPackage, InvoicePackage
 
 
-#@admin.register(Package)
-#class PackageAdmin(admin.ModelAdmin):
-#    list_display = ['id_package', 'name', 'active']
+@admin.register(Package)
+class PackageAdmin(admin.ModelAdmin):
+    list_display = ['id_package', 'name', 'active']
     
-#@admin.register(PackageDiscount)
-#class PackageDiscountAdmin(admin.ModelAdmin):
-#    list_display = ['id_package_discount', 'discount_rate', 'active', 'id_package']
+@admin.register(PackageDiscount)
+class PackageDiscountAdmin(admin.ModelAdmin):
+    list_display = ['id_package_discount', 'discount_rate', 'active', 'id_package']
+
+@admin.register(PackageDiscountPackage)
+class PackageDiscountPackageAdmin(admin.ModelAdmin):
+    list_display = ['id_package_discount_package', 'id_package', 'id_package_discount']
+
+@admin.register(InvoicePackage)
+class InvoicePackageAdmin(admin.ModelAdmin):
+    list_display = ['id_invoice_package', 'id_customer', 'id_package', 'final_package_price']
 
