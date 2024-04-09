b232b516bbe8c47e521212cfb531066487a332bb
inessdecastro <inessdecastro@gmail.com>
2024-04-09 11:21:24 +0100

    Operators views packages and services

--- project/operators/views.py
+++ project/operators/views.py
@@ -4,6 +4,10 @@ from .forms import OperatorsForm
 from .models import Operators
 from django.shortcuts import get_object_or_404
 from django.urls import reverse_lazy
+from packages.models import Package, PackageDiscount
+from services.models import Service, ServiceDiscount
+from packages.forms import PackageForm, PackageDiscountForm
+from services.forms import ServiceForm, ServiceDiscountForm
 
 class OperatorsCreateView(CreateView):
     template_name = 'operators_create.html'
@@ -73,64 +77,64 @@ class MenuDiscounts(TemplateView):
 
 #Views operators packages and services
 
-#class AssignPackageView(UpdateView):
-    #template_name = 'assign_package.html'
-    #form_class = AssignPackageForm
-    #model = Package
-    #success_url = reverse_lazy('operators_list')
+class AssignPackageView(UpdateView):
+    template_name = 'assign_package.html'
+    form_class = PackageForm
+    model = Package
+    success_url = reverse_lazy('operators_list')
 
-    #def form_valid(self, form):
+    def form_valid(self, form):
         # Atribuir um pacote comercial a um cliente
-        #return super().form_valid(form)
+        return super().form_valid(form)
 
-#class AssignPackageDiscountView(UpdateView):
-    #template_name = 'assign_package_discount.html'
-    #form_class = AssignPackageDiscountForm
-    #model = Package_Discount
-    #success_url = reverse_lazy('operators_list')
+class AssignPackageDiscountView(UpdateView):
+    template_name = 'assign_package_discount.html'
+    form_class = PackageDiscountForm
+    model = PackageDiscount
+    success_url = reverse_lazy('operators_list')
 
-    #def form_valid(self, form):
+    def form_valid(self, form):
         # Atribuir uma promoção a um pacote comercial
-        #return super().form_valid(form)
+        return super().form_valid(form)
 
-#class PackageListView(ListView):
-    #model = Package
-    #template_name = 'package_list.html'
-    #context_object_name = 'package_list'
+class PackageListView(ListView):
+    model = Package
+    template_name = 'package_list.html'
+    context_object_name = 'package_list'
 
-#class PackageDiscountListView(ListView):
-    #model = Package_Discount
-    #template_name = 'package_dicount_list.html'
-    #context_object_name = 'package_discount_list'
+class PackageDiscountListView(ListView):
+    model = PackageDiscount
+    template_name = 'package_dicount_list.html'
+    context_object_name = 'package_discount_list'
     
-#
-# class AssignServiceView(UpdateView):
-#     template_name = 'assign_service.html'
-#     form_class = AssignServiceForm
-#     model = Service
-#     success_url = reverse_lazy('operators_list')
-#
-#     def form_valid(self, form):
-#         # Atribuir um serviço a um cliente
-#         return super().form_valid(form)
-#
-# class AssignServiceDiscountView(UpdateView):
-#     template_name = 'assign_service_discount.html'
-#     form_class = AssignServiceDiscountForm
-#     model = Service_Discount
-#     success_url = reverse_lazy('operators_list')
-#
-#     def form_valid(self, form):
-#         # Atribuir uma promoção a um serviço
-#         return super().form_valid(form)
-#
-# class ServiceListView(ListView):
-#     model = Service
-#     template_name = 'service_list.html'
-#     context_object_name = 'service_list'
-#
-# class ServiceDiscountListView(ListView):
-#     model = Service_Discount
-#     template_name = 'service_discount_list.html'
-#     context_object_name = 'service_discount_list'
+
+class AssignServiceView(UpdateView):
+    template_name = 'assign_service.html'
+    form_class = ServiceForm
+    model = Service
+    success_url = reverse_lazy('operators_list')
+
+    def form_valid(self, form):
+        # Atribuir um serviço a um cliente
+        return super().form_valid(form)
+
+class AssignServiceDiscountView(UpdateView):
+    template_name = 'assign_service_discount.html'
+    form_class = ServiceDiscountForm
+    model = ServiceDiscount
+    success_url = reverse_lazy('operators_list')
+
+    def form_valid(self, form):
+         # Atribuir uma promoção a um serviço
+        return super().form_valid(form)
+
+class ServiceListView(ListView):
+    model = Service
+    template_name = 'service_list.html'
+    context_object_name = 'service_list'
+
+class ServiceDiscountListView(ListView):
+    model = ServiceDiscount
+    template_name = 'service_discount_list.html'
+    context_object_name = 'service_discount_list'
     
