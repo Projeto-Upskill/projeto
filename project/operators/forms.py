ffb8fd8a30dc6925fefe47817a927bb2a50b7abc
henriquealvesgonzaga87 <henriquealves87@hotmail.com>
2024-04-14 12:48:27 +0100

    add an user and split name to 1º and last name on the operators models and views

--- project/operators/forms.py
+++ project/operators/forms.py
@@ -1,34 +1,45 @@
 from django import forms
-from .models import Operators
+from django.contrib.auth.models import User
 
 
 class DateInput(forms.DateInput):
     input_type = "date"
 
 
+class PasswordInput(forms.PasswordInput):
+    input_type = 'password'
+
+
 class OperatorsForm(forms.ModelForm):
-    name = forms.CharField(max_length=500)
+    first_name = forms.CharField(max_length=500)
+    last_name = forms.CharField(max_length=500)
     email = forms.EmailField(max_length=300)
     birth_date = forms.DateField(widget=DateInput())
     admission_date = forms.DateField(widget=DateInput())
     active = forms.BooleanField()
+    username = forms.CharField(max_length=100)
+    password = forms.CharField(widget=PasswordInput())
     
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.fields['email'].required = True
-        self.fields['name'].required = True
+        self.fields['first_name'].required = True
+        self.fields['last_name'].required = True
         self.fields['birth_date'].required = True
         self.fields['admission_date'].required = True
         self.fields['active'].required = True
 
     class Meta:
-        model = Operators
+        model = User
         fields = (
-            'name',
+            'first_name',
+            'last_name',
             'email',
             'birth_date',
             'admission_date',
             'active',
+            'username',
+            'password'
 
         )
 
