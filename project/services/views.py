55d4c8ddbf9ab6205475a141837b2ce4ee88c0ea
henriquealvesgonzaga87 <henriquealves87@hotmail.com>
2024-04-20 20:10:46 +0100

    creates the customerservice create view

--- project/services/views.py
+++ project/services/views.py
@@ -1,7 +1,7 @@
 from django.views.generic import CreateView, ListView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from .models import ServiceType, Service, ServiceDiscount, ServiceDiscountService, InvoiceService, ServiceCustomer
-from .forms import ServiceTypeForm, ServiceForm, ServiceDiscountForm, ServiceDiscountServiceForm, InvoiceServiceForm
+from .forms import ServiceTypeForm, ServiceForm, ServiceDiscountForm, ServiceDiscountServiceForm, InvoiceServiceForm, CustomerServiceForm
 from django.shortcuts import get_object_or_404, redirect
 from django.contrib.auth.mixins import PermissionRequiredMixin, LoginRequiredMixin
 from django.db.models import Q
@@ -199,6 +199,13 @@ class ServiceDiscountServiceDeleteView(PermissionRequiredMixin, LoginRequiredMix
     def get_object(self):
         id = self.kwargs.get('id_service_discount_service')
         return get_object_or_404(ServiceDiscountService, id_service_discount_service=id)
+
+
+class ServiceCustomerCreateView(CreateView):
+    model = ServiceCustomer
+    form_class = CustomerServiceForm
+    template_name = 'service_customer_create.html'
+    success_url = reverse_lazy('administrator:menu_services')
 
 
 class ServiceCustomerListView(LoginRequiredMixin, PermissionRequiredMixin, ListView):
